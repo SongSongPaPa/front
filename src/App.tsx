86fcc195080bf697bf1de85a6f93b6395cb0ca79
sohan <sohan@student.42seoul.kr>
2023-03-05 15:03:33 +0900

    Feat: add Form type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './LoginPage';
+import { TextInput, Login } from './LoginPage';
 
 /*function App() {
   return (
@@ -33,6 +34,8 @@ function App() {
   return (
     <div className="App">
       <LoginPage loginUri="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-3caeb9dbdfcb3e93713d5075212189e056e453825da4e6a3c108ebba393824c9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code" onLogin={handleLogin} />
+      <TextInput></TextInput>
+      <Login></Login>
     </div>
   );
 };
--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { request } from 'http';
+import './button.css';
 
 interface LoginPageProps {
   onLogin: () => void;
@@ -9,8 +12,10 @@ function LoginPage(props:LoginPageProps) {
 	const { loginUri } = props;
 
 	const handleLogin = () => {
-		window.location.href = loginUri;
-	}
+		//window.location.href = loginUri;
+    axios.get("http://10.11.1.7:3000/echo/seunpark babo")
+    .then(response => {console.log(response.data)});
+  }
 
   return (
     <div>
@@ -20,5 +25,73 @@ function LoginPage(props:LoginPageProps) {
   );
 };
 
+function TextInput() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setText(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('input text:', text);
+    axios.get("http://10.11.1.7:3000/redirection/")
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter some text:
+        <input type="text" value={text} onChange={handleChange}/>
+      </label>
+      <button className="button" type="submit">Submit</button>
+    </form>
+  );
+}
+
+function Login(){
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleIdChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setId(event.currentTarget.value);
+  }
+
+  const handlePwChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //console.log('input text:', text);
+    axios.post("http://10.11.1.7:3000/", {id , password})
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  };
+
+  return (
+    <form onSubmit={handleLoginSubmit}>
+      <label>
+        Id:
+        <input type="text" value={id} onChange={handleIdChange}/>
+        Pw:
+        <input type="text" value={password} onChange={handlePwChange}/>
+      </label>
+      <button className="button" type="submit">Submit</button>
+    </form>
+  );
+}
+
 export default LoginPage;
+export { TextInput, Login };
 
