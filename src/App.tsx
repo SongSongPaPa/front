a026ac2cc983ea9c262600ce5785f6b016e8a436
sohan <sohan@student.42seoul.kr>
2023-03-13 19:20:36 +0900

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 import LoginPage from './LoginPage';
 import { TextInput, Login } from './LoginPage';
 
@@ -26,18 +26,30 @@ import { TextInput, Login } from './LoginPage';
 }*/
 
 function App() {
-  const handleLogin = () => {
+  /*const handleLogin = () => {
     // handle login logic here
     console.log('User logged in!');
-  };
-
+    const currentUri = window.location.href;
+    console.log(currentUri);
+  };*/
+  const uri = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-3caeb9dbdfcb3e93713d5075212189e056e453825da4e6a3c108ebba393824c9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code";
   return (
     <div className="App">
-      <LoginPage loginUri="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-3caeb9dbdfcb3e93713d5075212189e056e453825da4e6a3c108ebba393824c9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code" onLogin={handleLogin} />
-      <TextInput></TextInput>
-      <Login></Login>
+      <LoginPage loginUri={uri} />
     </div>
   );
 };
 
+//import React from 'react';
+//import LoginPage from './LoginPage';
+
+/*function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}*/
+
 export default App;
+
--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { request } from 'http';
 import './button.css';
 
 interface LoginPageProps {
-  onLogin: () => void;
+  //onLogin: () => void;
   loginUri: string;
 }
 
@@ -12,11 +12,18 @@ function LoginPage(props:LoginPageProps) {
 	const { loginUri } = props;
 
 	const handleLogin = () => {
-		//window.location.href = loginUri;
-    axios.get("http://10.11.1.7:3000/echo/seunpark babo")
-    .then(response => {console.log(response.data)});
+		window.location.replace(loginUri);
+    //axios.get("http://10.11.1.7:3000/echo/seunpark babo")
+    //.then(response => {console.log(response.data)});
   }
+   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
 
+    if (code){
+      console.log(code);
+    }
+  }, []);
   return (
     <div>
       <h1>Login Page</h1>
@@ -95,3 +102,64 @@ function Login(){
 export default LoginPage;
 export { TextInput, Login };
 
+//import React, { useState } from 'react';
+/*import './LoginPage.css';
+import './Login.css';
+
+interface LoginProps {
+  onSubmit: (username: string, password: string) => void;
+}
+
+function Login({ onSubmit }: LoginProps) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(username, password);
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+function LoginPage() {
+  const handleLogin = (username: string, password: string) => {
+    // 로그인 처리를 수행
+    console.log('username:', username);
+    console.log('password:', password);
+  };
+
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+      <div className="form-container">
+        <Login onSubmit={handleLogin} />
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
+*/
