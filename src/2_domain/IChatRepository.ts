51e6e04ada87f86c0d20b24dc7be0d83ae41d78f
Seunghan Park <seunpark@student.42seoul.kr>
2023-04-30 20:23:14 +0900

    Feat: Chat Repository 생성

--- src/2_domain/IChatRepository.ts
+++ src/2_domain/IChatRepository.ts
@@ -2,5 +2,28 @@ import { PublicUserInfo } from "./User";
 import { PublicChatInfo } from "./Chat";
 
 export interface IChatRepository {
-  createChatRoom(): void;
+  /* ================================= */
+  /*             Broadcast             */
+  /* ================================= */
+
+  createChatRoom(name: string, type: string, password?: string): void;
+  updateChat(name: string, type: string, password?: string): void;
+  setAdmin(userId: number): void;
+
+  /* ============================= */
+  /*             Group             */
+  /* ============================= */
+
+  joinChat(userId: number, password?: string): void;
+  leaveChat(): void;
+  sendMessage(message: string): void;
+  kickUser(userId: number): void;
+  muteUser(userId: number): void;
+
+  /* ============================== */
+  /*             Single             */
+  /* ============================== */
+
+  sendDirectMessage(userId: number, message: string): void;
+  inviteUser(userId: number): void;
 }
--- src/3_infrastructure/SocketIOChatRepository.ts
+++ src/3_infrastructure/SocketIOChatRepository.ts
@@ -12,13 +12,58 @@ class ChatRepository implements IChatRepository {
 
   /*public setSocket(serverUrl: string) {
     this.socket = io(serverUrl);
-
-    
   }*/
 
-  public createChatRoom(): void {
-    console.log("hey");
-    this.socket.emit("createChat", { name: "asdf", type: "public" });
+  /* ================================= */
+  /*             Broadcast             */
+  /* ================================= */
+
+  public createChatRoom(name: string, type: string, password?: string): void {
+    this.socket.emit("createChat", { name: name, type: type, password: password });
+  }
+
+  public updateChat(name: string, type: string, password?: string): void {
+    this.socket.emit("updateChat", { name: name, type: type, password: password });
+  }
+
+  public setAdmin(userId: number): void {
+    this.socket.emit("setAdmin", { userId: userId });
+  }
+
+  /* ============================= */
+  /*             Group             */
+  /* ============================= */
+
+  public joinChat(userId: number, password?: string): void {
+    this.socket.emit("joinChat", { userId: userId, password: password });
+  }
+
+  public leaveChat(): void {
+    this.socket.emit("leaveChat");
+  }
+
+  public sendMessage(message: string): void {
+    this.socket.emit("sendMessage", { message: message });
+  }
+
+  public kickUser(userId: number): void {
+    this.socket.emit("kickUser", { userId: userId });
+  }
+
+  public muteUser(userId: number): void {
+    this.socket.emit("muteUser", { userId: userId });
+  }
+
+  /* ============================== */
+  /*             Single             */
+  /* ============================== */
+
+  public sendDirectMessage(userId: number, message: string): void {
+    this.socket.emit("sendDirectMessage", { userId: userId, message: message });
+  }
+
+  public inviteUser(userId: number): void {
+    this.socket.emit("inviteUser", { userId: userId });
   }
 }
 
