0e1261bee3e8a4abfea631a91bedd715567336c1
sohan <sohan@student.42seoul.kr>
2023-05-06 01:17:50 +0900

    Test: 프로필 이미지 파일 base64 인코딩으로 emit

--- src/0_presentation/User/Profile.tsx
+++ src/0_presentation/User/Profile.tsx
@@ -3,10 +3,22 @@ import useUserService from "@root/1_application/useUserService";
 import { useRecoilValue } from "recoil";
 import { meState } from "@root/2_domain/recoil/userAtom";
 import Input from "../components/common/Input";
+import GlobalSocket from "@root/3_infrastructure/GlobalSocket";
 
 const Profile = () => {
   const { getMyProfile } = useUserService();
   const profile = useRecoilValue(meState);
+
+  const handleOnImageFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const socket = GlobalSocket.getSocket();
+    const file = event.target.files![0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (curr) => {
+      const base64 = curr.target!.result;
+      socket.emit("updateImage", { image: base64 });
+    };
+  };
   useEffect(() => {
     getMyProfile();
   }, []);
@@ -19,7 +31,7 @@ const Profile = () => {
         <p>{profile.name}</p>
         <p>{profile.level}</p>
         <Input name="default" type="flle" accept="image/*" />
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" onChange={handleOnImageFileChange} />
       </li>
     </div>
   );
